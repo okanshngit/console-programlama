7ee3c1c33ea8eeb26f13f37b9794136d9eb76510
Okan Sahin <okan.shn@hotmail.com>
2022-01-31 15:14:56 +0300

    First commit.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken=null;
+            Console.WriteLine(degisken);
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+name  +" " +surname);
+            Console.Read();
+        }
+    }
+}
